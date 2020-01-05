5424a9cb7215d3e8cd7b2ce579ecbe4fcadf7914
Jared De la O <jareddelao@MacBook-Air-de-Jared.local>
2020-01-04 20:01:08 -0600

    login and register

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  login(correo: string, password: string) {
+    this.authService.login(correo, password);  
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCm-EkoPVMR1nKCxKvsi8Ic5Gkrvm6OKyQ',
+    authDomain: 'ingreso-egreso-app-97939.firebaseapp.com',
+    databaseURL: 'https://ingreso-egreso-app-97939.firebaseio.com',
+    projectId: 'ingreso-egreso-app-97939',
+    storageBucket: 'ingreso-egreso-app-97939.appspot.com',
+    messagingSenderId: '611444924838',
+    appId: '1:611444924838:web:c879b677eeafaa8e992f67'
+  }
 };
 
 /*
